9b3420f0efcac56e9a710b32eb9352f9a42ecd07
Adrian Menzel <menzeladrian@googlemail.com>
2017-05-26 16:26:05 +0200

    MVP - first usable version
    with avatar images and descriptions and stuff

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import $ from 'jquery';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+
+global.jQuery = $;
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
