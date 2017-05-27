cfd5e7b2555c436a5edb1691ac724ed3d6c5ae00
simmarith <menzeladrian@googlemail.com>
2017-05-28 00:31:52 +0200

    Fix dependencies
    removed useless import of jquery
    added node-trello to package.json

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import $ from 'jquery';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
-
-global.jQuery = $;
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
